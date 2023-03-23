fc4507cfd3840588b8c42f570d4f288dc344b68f
wei_acer <111753225@nccu.edu.tw>
2023-03-24 01:25:39 +0800

    Q2 modify safe ycbcr444 .yuv file

--- HW1/question_2.py
+++ HW1/question_2.py
@@ -3,9 +3,18 @@ import numpy as np
 from funtion import RGB2Ycbcr, Ycbcr444toYcbcr420_file, Ycbcr420_file2img ,Ycbcr2BGR
 
 frame_rgb = []
+file_ycbcr444 = open('question2_without_subsampling.yuv','wb')
+file_ycbcr444.truncate()
 for i in range(3):
     frame_rgb.append(cv.imread("foreman_qcif_"+ str(i) +"_rgb.bmp"))
-file_ycbcr420 = open('question2.yuv','wb')
+    (Y,cb,cr) = cv.split(frame_rgb[i])
+    file_ycbcr444.write(Y.tobytes())
+    file_ycbcr444.write(cb.tobytes())
+    file_ycbcr444.write(cr.tobytes())
+file_ycbcr444.seek(0)
+file_ycbcr444.close()
+file_ycbcr420 = open('question2_with_subsampling.yuv','wb')
+file_ycbcr420.truncate()
 for i in range(len(frame_rgb)):
     tmp_img = RGB2Ycbcr(frame_rgb[i])
     # cv.imshow('tmp_img',tmp_img)
